597a5ab30bb7ad9475f0e3ecbd4b40a09a111895
sferman05-maker <sferman05@gmail.com>
2026-06-08 11:33:05 -0500

    fix typescript build errors

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -79,8 +79,8 @@ function Browse() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {filteredDrinks.map(drink => {
             const brand = brands.find(b => b.slug === drink.brandSlug)
-            const caffeine = drink.caffeine ?? brand?.caffeine
-            const calories = drink.calories ?? brand?.calories
+            const _caffeine = drink.caffeine ?? brand?.caffeine
+            const _calories = drink.calories ?? brand?.calories
             return (
               <div
                 key={drink.id}
